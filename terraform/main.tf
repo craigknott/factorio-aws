481839983b5cadbfd3c00b4437fabc093456d024
Kurt Scherer <kurt.scherer@gmail.com>
2017-02-23 11:07:24 -0600

    Making AWS creds not a required variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 variable "name" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" { default = "" }
+variable "aws_secret_key" { default = "" }
 variable "tags" { type = "map" }
 variable "region" {}
 variable "vpc_cidr" {}
